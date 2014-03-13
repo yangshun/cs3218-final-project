3e213b08f1eb286766db4c81905ec7e61dd43609
Tay Yang Shun <tay.yang.shun@gmail.com>
2014-03-13 23:28:31 +0800

    Ninjas can shoot shurikens

--- Noisy Ninjas/Noisy Ninjas/Constants.h
+++ Noisy Ninjas/Noisy Ninjas/Constants.h
@@ -8,6 +8,13 @@
 
 #import <Foundation/Foundation.h>
 
+#define SCREEN_WIDTH 1024.f
+#define SCREEN_HEIGHT 768.f
 
+#define TIMER_INTERVAL 1.f/60.f
 
-#define MOVEMENT_DIST 10
+#define MOVEMENT_DIST 5
+#define NINJA_HEIGHT 100.f
+#define NINJA_WIDTH 89.f
+
+#define SHURIKEN_SIZE 30.f
--- Noisy Ninjas/Noisy Ninjas/ViewController.m
+++ Noisy Ninjas/Noisy Ninjas/ViewController.m
@@ -8,12 +8,14 @@
 
 #import "ViewController.h"
 #import "Constants.h"
+#import "AppDelegate.h"
 
 @interface ViewController () {
     NSTimer *gameTimer;
     UIImageView *ninja;
     BOOL updatePosition;
     BOOL directionIsUp;
+    NSMutableArray *shurikens;
 }
 
 @end
@@ -23,29 +25,59 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationNone];
+//    CGRect screenBounds = [[UIScreen mainScreen] bounds];
+//    self.view.frame = CGRectMake(0, 0, screenBounds.size.height,
+//                                 screenBounds.size.width);
+    [[UIApplication sharedApplication] setStatusBarHidden:YES
+                                            withAnimation:UIStatusBarAnimationNone];
     
-    gameTimer = [NSTimer scheduledTimerWithTimeInterval:0.067f
-                                        target:self
-                                      selector:@selector(updateNinjaPosition:)
-                                      userInfo:nil
-                                       repeats:YES];
-    [gameTimer fire];
+    gameTimer = [NSTimer scheduledTimerWithTimeInterval:TIMER_INTERVAL
+                                                 target:self
+                                               selector:@selector(tick)
+                                               userInfo:nil
+                                                repeats:YES];
     
     directionIsUp = YES;
     
     ninja = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"ninja-blue"]];
-    ninja.frame = CGRectMake(0, 0, 100.f, 89.f);
+    ninja.frame = CGRectMake(0, 0, NINJA_HEIGHT, NINJA_WIDTH);
     [self.view addSubview:ninja];
     
+    shurikens = [NSMutableArray new];
 }
-                 
-- (void)updateNinjaPosition:(id)sender {
+
+- (void)tick {
     if (updatePosition) {
-        int movement = directionIsUp ? -MOVEMENT_DIST : MOVEMENT_DIST;
-        ninja.center = CGPointMake(ninja.center.x,
-                                   ninja.center.y + movement);
+        [self updateNinjaPosition];
     }
+    
+    [self updateShurikensPosition];
+}
+
+- (void)updateNinjaPosition {
+    int movement = directionIsUp ? -MOVEMENT_DIST : MOVEMENT_DIST;
+    float newY = ninja.center.y + movement;
+    newY = MIN(newY, SCREEN_HEIGHT - NINJA_HEIGHT/2);
+    newY = MAX(NINJA_HEIGHT/2, newY);
+    ninja.center = CGPointMake(ninja.center.x, newY);
+}
+
+- (void)updateShurikensPosition {
+    
+    NSMutableArray *outOfBoundsShurikens = [NSMutableArray new];
+    
+    for (UIImageView *shuriken in shurikens) {
+        shuriken.center = CGPointMake(shuriken.center.x + 20.f,
+                                      shuriken.center.y);
+        shuriken.transform = CGAffineTransformRotate(shuriken.transform, 10.f);
+        
+        if (shuriken.center.x > SCREEN_WIDTH) {
+            [outOfBoundsShurikens addObject:shuriken];
+            [shuriken removeFromSuperview];
+        }
+    }
+    
+    [shurikens removeObjectsInArray:outOfBoundsShurikens];
 }
 
 - (void)didReceiveMemoryWarning
@@ -69,7 +101,11 @@
 }
 
 - (IBAction)shoot:(id)sender {
-    NSLog(@"shoot");
+    UIImageView *shuriken = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"shuriken-4-point-star"]];
+    shuriken.frame = CGRectMake(0, 0, SHURIKEN_SIZE, SHURIKEN_SIZE);
+    shuriken.center = CGPointMake(ninja.center.x + 40.f, ninja.center.y);
+    [shurikens addObject:shuriken];
+    [self.view addSubview:shuriken];
 }
 
 @end
